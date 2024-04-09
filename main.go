e24f35f9bf8a918fac62bedf89deea17b13a56d9
JesseKoldewijn <jesse.koldewijn@denieuwezaak.nl>
2024-04-09 15:00:27 +0200

    fix: added ENV based port flow

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"os"
 	"time"
 
 	v1 "github.com/JesseKoldewijn/go-jereko-api/routes/v1"
@@ -9,6 +10,17 @@ import (
 )
 
 func main() {
+	env_vars := os.Environ()
+
+	var PORT = "8080"
+
+	// get PORT env var
+	for _, env_var := range env_vars {
+		if env_var[:4] == "PORT" {
+			PORT = env_var[5:]
+		}
+	}
+
 	router := gin.Default()
 
 	router.GET("/", func(c *gin.Context) {
@@ -20,14 +32,16 @@ func main() {
 	v1.RouteGroupV1(router)
 
 	s := &http.Server{
-		Addr:           ":8080",
+		Addr:           ":" + string(PORT),
 		Handler:        router,
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
 	}
 
-	println("Server running on: http://localhost:8080")
+	url := "http://localhost:" + string(PORT)
+
+	println("Server running on: " + url)
 
 	s.ListenAndServe()
 }
