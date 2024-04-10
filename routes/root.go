b7f35020e59c0661260f9a0dc78b021b36fc9d5b
JesseKoldewijn <jesse.koldewijn@denieuwezaak.nl>
2024-04-10 12:27:31 +0200

    feat: added static assets dir + router

--- routes/root.go
+++ routes/root.go
@@ -1,6 +1,8 @@
 package routes
 
 import (
+	"net/http"
+
 	utils "github.com/JesseKoldewijn/go-jereko-api/routes/api/utils"
 	v1 "github.com/JesseKoldewijn/go-jereko-api/routes/api/v1"
 	"github.com/gin-gonic/gin"
@@ -8,10 +10,18 @@ import (
 
 // The function `RouteGroupRoot` sets up route groups with different prefixes in a Gin router.
 func RouteGroupRoot(router *gin.Engine) {
+	// prefix /assets
+	routeGroupStatic(router)
 	// prefix /
 	routeGroupCore(router)
 	// prefix /api
 	routeGroupApi(router)
+}
+
+// Route group for static file routes
+func routeGroupStatic(router *gin.Engine) {
+	staticGroup := router.Group("/assets")
+	staticGroup.StaticFS("/", http.Dir("./public"))
 }
 
 // Route group for general routes
