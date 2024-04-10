7e75eb88ed827583ef72ec7b78a8828408677fc3
JesseKoldewijn <jesse.koldewijn@denieuwezaak.nl>
2024-04-10 08:33:25 +0200

    feat: added post body ping-pong route + small tweak of api core

--- main.go
+++ main.go
@@ -5,32 +5,40 @@ import (
 	"os"
 	"time"
 
+	"github.com/fvbock/endless"
+
 	v1 "github.com/JesseKoldewijn/go-jereko-api/routes/v1"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	// Get all environment variables
 	env_vars := os.Environ()
 
-	var PORT = "8080"
+	// Default values
+	PORT := "8080"
 
-	// get PORT env var
+	// Get PORT env var
 	for _, env_var := range env_vars {
 		if env_var[:4] == "PORT" {
 			PORT = env_var[5:]
 		}
 	}
 
+	// Create router
 	router := gin.Default()
 
+	// Default route
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "Welcome to the Jereko API!",
 		})
 	})
 
+	// Routes
 	v1.RouteGroupV1(router)
 
+	// Create server
 	s := &http.Server{
 		Addr:           ":" + string(PORT),
 		Handler:        router,
@@ -39,9 +47,17 @@ func main() {
 		MaxHeaderBytes: 1 << 20,
 	}
 
-	url := "http://localhost:" + string(PORT)
+	// Server address
+	port := ":" + string(PORT)
+	url := "http://localhost" + port
 
 	println("Server running on: " + url)
 
-	s.ListenAndServe()
+	// Listen and serve
+	err := endless.ListenAndServe(port, s.Handler)
+
+	// Check for errors
+	if err != nil {
+		println("Error starting server: ", err)
+	}
 }
